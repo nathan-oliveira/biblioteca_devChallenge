e1dc0b97569c89a1135fa2f7c2a474bfa2461ce2
nathan-oliveira <nathan_oliveiramendonca@hotmail.com>
2020-09-14 18:06:32 -0300

    ajuste

--- src/bin/app.ts
+++ src/bin/app.ts
@@ -20,7 +20,7 @@ class App {
       allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
       credentials: true,
       methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-      origin: 'http://localhost:3000',
+      origin: 'http://localhost:8080',
       preflightContinue: false
     };
 
